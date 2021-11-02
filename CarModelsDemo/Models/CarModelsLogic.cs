8d990f95aff1e41ac823d206ecf11f4e9d4f15e7
Anh-Toine <Nick1234567890a@gmail.com>
2021-11-02 15:07:19 -0400

    First commit

--- CarModelsDemo/Models/CarModelsLogic.cs
+++ CarModelsDemo/Models/CarModelsLogic.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CarModelsDemo.Models
 {
+    [Table("CarModels")]
     public class CarModelsLogic
     {
-        public CarModelsLogic()
-        {
-        }
+        [Key]
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Company { get; set; }
+        public string Release { get; set; }
+        public string Country { get; set; }
     }
 }
